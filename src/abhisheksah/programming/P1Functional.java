15d8b84f022d80de47798421c8055d451381c5b9
abhisheksah255 <abhisheksah255@gmail.com>
2024-08-21 02:13:36 +0530

    Add one more method to save only even numbers

--- src/abhisheksah/programming/P1Functional.java
+++ src/abhisheksah/programming/P1Functional.java
@@ -4,20 +4,27 @@ import java.util.List;
 
 public class P1Functional {
 	
-	private static void print(int number) {
-		System.out.println(number);
-	}
+//	private static void print(int number) {
+//		System.out.println(number);
+//	}
 
 	public static void main(String[] args) {
 
 	
-	
-		printAllNumberInListFunctional(List.of(34,56,78,54,23,56,34,67,48,54,22,23,54));
+		List<Integer> numbers = List.of(34,56,78,54,53,56,33,67,48,45,21,23,54);
+		printAllNumberInListFunctional(numbers);
+		System.out.println("*********");
+		printAllEvenNumberInListFunctional(numbers);
 	}
+
+	private static void printAllEvenNumberInListFunctional(List<Integer> numbers) {
+
+		numbers.stream().filter(n ->n%2==0).forEach(System.out::println);
+}
 
 	private static void printAllNumberInListFunctional(List<Integer> numbers) {
 
-		numbers.stream().forEach(P1Functional::print);//method Reference
+		numbers.stream().forEach(System.out::println);//method Reference
 	}
 
 }
--- src/abhisheksah/programming/P1Structured.java
+++ src/abhisheksah/programming/P1Structured.java
@@ -7,13 +7,25 @@ public class P1Structured {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		printAllNumberInListStructured(List.of(34,56,78,54,23,56,34,67,48,54,22,23,54));
+		List<Integer> numbers = List.of(34,56,78,54,53,56,33,67,48,54,21,23,54);
+		printAllNumberInListStructured(numbers);
+		System.out.println("******************");
+		printAllEvenNumbersInListStructured(numbers);
 	}
 
 	private static void printAllNumberInListStructured(List<Integer> numbers) {
 
 		for(int number :numbers) {
 			System.out.println(number);
+		}
+	}
+	
+	private static void printAllEvenNumbersInListStructured(List<Integer> numbers) {
+
+		for(int number :numbers) {
+			if(number%2==0) {
+			System.out.println(number);
+		}
 		}
 	}
 
