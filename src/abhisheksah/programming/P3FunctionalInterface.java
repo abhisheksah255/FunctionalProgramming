509de7a5fcb1e2c74e17b8ebeccd95f6b3d2ffc0
abhisheksah255 <abhisheksah255@gmail.com>
2024-08-25 15:53:39 +0530

    Lambda function working

---
+++ src/abhisheksah/programming/P3FunctionalInterface.java
@@ -0,0 +1,57 @@
+package abhisheksah.programming;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class P3FunctionalInterface {
+	
+//	private static boolean isEven(int x) {
+//		return x%2==0;
+//	}
+//	private static int square(int x) {
+//		return x*x ;
+//	}
+
+	public static void main(String[] args) {
+
+	
+		List<Integer> numbers = List.of(34,56,78,54,53,56,33,67,48,45,21,23,54);
+		
+		Predicate<Integer> isEvenpredicate = x ->x%2==0;
+		Predicate<Integer> isEvenpredicate1 = new Predicate<Integer>(){
+
+			@Override
+			public boolean test(Integer x) {
+				return x%2==0;
+			}
+			
+		};
+		
+		Function<Integer, Integer> squaremapper = x ->x*x;
+		Function<Integer, Integer> squaremapper1 =new Function<Integer, Integer> (){
+
+			@Override
+			public Integer apply(Integer x) {
+				return x*x;
+			}
+			};
+		
+		Consumer<Integer> printaction = System.out::println;
+		Consumer<Integer> printaction2 = new Consumer<Integer>()
+				{
+			public void accept(Integer x) {
+				System.out.println(x);
+			}
+		};
+		
+		
+		numbers.stream().
+		filter(isEvenpredicate).
+		map(squaremapper).
+		forEach(printaction);
+		
+	}
+
+}
